779f76d2ea1e6fb252eda828eb84043c76ca3666
Hazar Saad ElDine <saadeldinehazar@gmail.com>
2021-04-30 23:17:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 function findMatching(drivers,string){
   return drivers.filter(name => { return (name.toLowerCase()===string.toLowerCase())})
 }
+
+function fuzzyMatch(drivers,string){
+  return drivers.filter(name => {
+     if (string.charAt(0) === name.charAt(0))
+     {return name;}
+   }
+   )
+}
